0f54bef0223f6d39f452f7b38f2d22633a879d39
Sebastian <cbastian97araque@gmail.com>
2020-10-11 14:44:54 -0500

    se agrega archivo JavaScript (efecto scrollReveal)

---
+++ main.js
@@ -0,0 +1,13 @@
+document.querySelector('.menu-boton').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.projects', { delay: 500 });
+ScrollReveal().reveal('.my-experience', { delay: 500 });
+ScrollReveal().reveal('.projects', { delay: 500 });
+ScrollReveal().reveal('.social', { delay: 500 });
+ScrollReveal().reveal('.footer-one', { delay: 500 });
+ScrollReveal().reveal('.final-links', { delay: 500 });
+ScrollReveal().reveal('.formulario', { delay: 500 });
+ScrollReveal().reveal('.footer', { delay: 300 });
