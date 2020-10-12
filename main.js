1fe8290a71b10602600e0fc77954a01ba643132c
Sebastian <cbastian97araque@gmail.com>
2020-10-11 22:18:30 -0500

    se hicieron cambios como, scrollreveal, fuentes, y logos

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ document.querySelector('.menu-boton').addEventListener('click', () => {
 });
 
 ScrollReveal().reveal('.showcase');
-ScrollReveal().reveal('.projects', { delay: 500 });
-ScrollReveal().reveal('.my-experience', { delay: 500 });
-ScrollReveal().reveal('.projects', { delay: 500 });
-ScrollReveal().reveal('.social', { delay: 500 });
-ScrollReveal().reveal('.footer-one', { delay: 500 });
-ScrollReveal().reveal('.final-links', { delay: 500 });
-ScrollReveal().reveal('.formulario', { delay: 500 });
-ScrollReveal().reveal('.footer', { delay: 300 });
+ScrollReveal().reveal('.projects', { delay: 600 });
+ScrollReveal().reveal('.scroll1', { delay: 650 });
+ScrollReveal().reveal('.scroll2', { delay: 1100 });
+ScrollReveal().reveal('.scroll3', { delay: 1600 });
+ScrollReveal().reveal('.my-experience', { delay: 600 });
+ScrollReveal().reveal('.social', { delay: 1100 });
+ScrollReveal().reveal('.footer-one', { delay: 600 });
+ScrollReveal().reveal('.final-links', { delay: 600 });
+ScrollReveal().reveal('.formulario', { delay: 600 });
+ScrollReveal().reveal('.formulario', { delay: 600 });
+ScrollReveal().reveal('.footer', { delay: 200 });
